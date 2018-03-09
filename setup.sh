3ea15af1d6a690151a7b8c42cbb717a993dbec2d
Kshitij Karthick <kshitij.karthick@gmail.com>
2018-03-09 18:26:28 +0530

    Setup: Add script to help with installation and starting the coding environment.

---
+++ setup.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+NC='\033[0m'
+
+install_core_dependencies() {
+    echo -e "${RED}Installing Core Dependencies${NC}"
+    sudo apt-get update
+    sudo apt-get install python-dev
+    sudo apt-get install python-pip
+    sudo pip install --upgrade pip
+    sudo -H pip install virtualenv
+}
+
+install_python_dependencies() {
+    echo -e "${RED}Installing Python Dependencies${NC}"
+    pip install -r requirements.txt
+}
+
+create_python_environment() {
+    echo -e "${RED}Creating Python Environment${NC}"
+    virtualenv venv
+}
+
+activate_python_enviroment() {
+    echo -e "${RED}Activating Python Environment${NC}"
+    source venv/bin/activate
+}
+
+deactivate_python_environment() {
+    echo -e "${RED}Deactivating Python Environment${NC}"
+    deactivate
+}
+
+setup_environment() {
+    install_core_dependencies
+    clean
+    create_python_environment
+    activate_python_enviroment
+    install_python_dependencies
+    deactivate_python_environment
+}
+
+clean() {
+    echo -e "${RED}Cleaning Environment${NC}"
+    rm -rf venv
+}
+
+start_jupyter_environment() {
+    activate_python_enviroment
+    echo -e "${RED}Starting Jupyter Notebook${NC}"
+    jupyter notebook
+    deactivate_python_environment
+}
+
+if [ "$1" == "install" ]; then
+    setup_environment
+elif [ "$1" == "jupyter" ]; then
+    start_jupyter_environment
+elif [ "$1" == "activate" ]; then
+    activate_python_enviroment
+elif [ "$1" == "deactivate" ]; then
+    deactivate_python_environment
+else
+    echo -e "Usage: ./setup.sh [options]"
+    echo -e "install\t\t: Install python and all dependencies required."
+    echo -e "jupyter\t\t: Start Jupyter notebook"
+    echo -e "activate\t: Activate python environment"
+    echo -e "deactivate\t: Deactivate python environment"
+fi
